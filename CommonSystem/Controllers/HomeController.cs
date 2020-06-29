3e28ab0c9760afd8ba82073795c6a9ffe8a39cd2
1265093215 <1265093215@qq.com>
2020-06-29 21:51:27 +0800

    添加项目文件。

---
+++ CommonSystem/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CommonSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            _logger.LogError("111111111");
+            return View();
+        }
+    }
+}
