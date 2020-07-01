6eb82501e05b8d80274635bfc957353b01888d15
HWG <1265093215@qq.com>
2020-07-01 08:57:58 +0800

    Dapper帮助类

---
+++ Infrastructrue/DapperHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructrue
+{
+    public class DapperHelper
+    {
+        public void Dapper()
+        {
+
+        }
+    }
+}
